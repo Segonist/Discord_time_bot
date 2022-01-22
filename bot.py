0131b825c2b39ed5f32d7ad2f24066371af51e60
Segonist <74138556+Segonist@users.noreply.github.com>
2022-01-22 12:20:00 +0200

    Мелкие исправления

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import time_to_sec
 import user_data
 import config
 
-bot = commands.Bot(command_prefix=config.settings['PREFIX'], case_insensitive=True)  # префикс,
+bot = commands.Bot(command_prefix=config.settings['PREFIX'], case_insensitive=True)  # префикс, пох на регистр
 bot.remove_command('help')  # удаление команды help для создания своей
 
 
@@ -19,15 +19,16 @@ async def help(ctx):
                    '\nРазделы: '
                    '\nℹ!mhelp - Основная информация '
                    '\n📄!chelp - Команды бота '
-                   '\n✏!shelp - помощь по стилям времени '
-                   '\n🚫!ehelp - частые ошибки')  # команда !help
+                   '\n✏!shelp - Помощь по стилям временных меток '
+                   '\n🚫!ehelp - Частые ошибки')  # команда !help
 
 
 @bot.command()
 async def mhelp(ctx):
     return await ctx.send('>>> ℹ '
-                          '\nБот предназначен для создания дат следующего типа - <t:1656667800:f> '
-                          '\nТакая дата будет правильно отображаться у всех пользоваетелей, '
+                          '\nБот предназначен для создания временных меток следующего типа - <t:1656667800:f> '
+                          '\nТакая метка будет правильно отображаться у всех пользоваетелей, если часовой пояс '
+                          'установлен правильно '
                           '\nБот работает как на сервере, так и в личных сообщениях.')  # основная помощь
 
 
@@ -36,14 +37,16 @@ async def chelp(ctx):
     return await ctx.send('>>> 📄 '
                           '\nДля выбора часового пояса введите !utс "часовой пояс" '
                           '\nПример: !utc +2 '
-                          '\nДля перевода вашей даты в команду, используйте !time "год" "месяц" "день" "час" "минута" '
-                          '\nПример: !time 2022 01 06 15 20')  # помощь по командам
+                          '\nДля получения временной метки, используйте !time "год" "месяц" "день" "час" "минута" '
+                          '\nПример: !time 2022 01 06 15 20 '
+                          '\n чтобы ее использовать, надо скопировать полученный от бота '
+                          'текст и вставить в нужный чат')  # помощь по командам
 
 
 @bot.command()
 async def shelp(ctx):
     return await ctx.send('>>> ✏ '
-                          '\nУ времени в дискорде есть 7 стилей отображения. '
+                          '\nУ временных меток в дискорде есть 7 стилей отображения. '
                           '\nВнизу указан символ который надо указывать в !style, и пример отображения'
                           '\nt   <t:1656667800:t> '
                           '\nT  <t:1656667800:T> '
@@ -85,18 +88,18 @@ async def utc(ctx, timezone=None):  # команда !utc
 
 
 @bot.command()
-async def style(ctx, style=None):  # команда style
+async def style(ctx, format_style=None):  # команда style
     author = ctx.message.author  # автор сообщения
-    if style is None:  # если сообщение без аргументов
+    if format_style is None:  # если сообщение без аргументов
         if user_data.users_style.get(author) is None:  # если стиль не задан
             await ctx.send('Стиль не задан. Подробнее: !shelp')
         else:
-            style = user_data.users_style[author]  # получение часового пояса
-            await ctx.send('Ваш стиль: ' + style + '. Пример: <t:1656667800:' + style + '>')
+            format_style = user_data.users_style[author]  # получение часового пояса
+            await ctx.send('Ваш стиль: ' + format_style + '. Пример: <t:1656667800:' + format_style + '>')
     else:
-        user_data.users_style.update({author: style})  # запись стиля в файл
-        print('%s style:%s' % (author, style))  # большой брат следит за тобой
-        await ctx.send('Пример выбранного стиля: <t:1656667800:' + style + '>')  # сообщение с примером
+        user_data.users_style.update({author: format_style})  # запись стиля в файл
+        print('%s style:%s' % (author, format_style))  # большой брат следит за тобой
+        await ctx.send('Пример выбранного стиля: <t:1656667800:' + format_style + '>')  # сообщение с примером
 
 
 @bot.command(name='time')  # команда !time
@@ -107,9 +110,9 @@ async def _time(ctx, user_year, user_month, user_day, user_hour, user_minute):
     else:
         user_utc = user_data.users_utc[author]  # получение часового пояса
     if user_data.users_style.get(author) is None:
-        style = 'f'
+        format_style = 'f'
     else:
-        style = user_data.users_style[author]
+        format_style = user_data.users_style[author]
     try:
         split = list(user_utc)  # разделение UTC на знак и значение
     except TypeError:
@@ -117,10 +120,10 @@ async def _time(ctx, user_year, user_month, user_day, user_hour, user_minute):
     try:
         if split[0] == '+':
             number_to_withdraw = time_to_sec.discord_time_plus(user_year, user_month, user_day, user_hour, user_minute,
-                                                               int(split[1]), style)  # если UTC положительный
+                                                               int(split[1]), format_style)  # если UTC положительный
         elif split[0] == '-':
             number_to_withdraw = time_to_sec.discord_time_minus(user_year, user_month, user_day, user_hour, user_minute,
-                                                                int(split[1]), style)  # если UTC отрицательный
+                                                                int(split[1]), format_style)  # если UTC отрицательный
     except UnboundLocalError:
         number_to_withdraw = time_to_sec.discord_time(user_year, user_month, user_day, user_hour,
                                                       user_minute, style)  # если UTC нулевой
--- time_to_sec.py
+++ time_to_sec.py
@@ -2,9 +2,9 @@ import datetime
 import time
 
 
-def discord_time_minus(user_year, user_month, user_day, user_hour, user_minute, utc, style):  # функция для орицательного UTC
-    time_transformed = datetime.datetime(int(user_year), int(user_month), int(user_day), int(user_hour),
-                                         int(user_minute))  # хз что это, но так надо
+def discord_time_minus(year, month, day, hour, minute, utc, style):  # функция для орицательного UTC
+    time_transformed = datetime.datetime(int(year), int(month), int(day), int(hour),
+                                         int(minute))  # хз что это, но так надо
     time_sec = int(time.mktime(time_transformed.timetuple()))  # время в секундах
     time_sec = time_sec - 7200  # перевод в нулевой UTC
     time_sec = time_sec - (utc * 3600)  # перевод часов в секунды и оптимизация под UTC
@@ -12,9 +12,9 @@ def discord_time_minus(user_year, user_month, user_day, user_hour, user_minute, 
     return number_to_withdraw  # возвращение результата
 
 
-def discord_time_plus(user_year, user_month, user_day, user_hour, user_minute, utc, style):  # функция для положительного UTC
-    time_transformed = datetime.datetime(int(user_year), int(user_month), int(user_day), int(user_hour),
-                                         int(user_minute))  # хз что это, но так надо
+def discord_time_plus(year, month, day, hour, minute, utc, style):  # функция для положительного UTC
+    time_transformed = datetime.datetime(int(year), int(month), int(day), int(hour),
+                                         int(minute))  # хз что это, но так надо
     time_sec = int(time.mktime(time_transformed.timetuple()))  # время в секундах
     time_sec = time_sec - 7200  # перевод в нулевой UTC
     time_sec = time_sec + (utc * 3600)  # перевод часов в секунды и оптимизация под UTC
@@ -22,9 +22,9 @@ def discord_time_plus(user_year, user_month, user_day, user_hour, user_minute, u
     return number_to_withdraw  # возвращение результата
 
 
-def discord_time(user_year, user_month, user_day, user_hour, user_minute, style):  # функция для нулевого UTC
-    time_transformed = datetime.datetime(int(user_year), int(user_month), int(user_day), int(user_hour),
-                                         int(user_minute))  # хз что это, но так надо
+def discord_time(year, month, day, hour, minute, style):  # функция для нулевого UTC
+    time_transformed = datetime.datetime(int(year), int(month), int(day), int(hour),
+                                         int(minute))  # хз что это, но так надо
     time_sec = int(time.mktime(time_transformed.timetuple()))  # время в секундах
     time_sec = time_sec - 7200  # перевод в нулевой UTC
     number_to_withdraw = '\<t:' + str(time_sec) + ':' + style + '>'  # добавление дискорд тега
