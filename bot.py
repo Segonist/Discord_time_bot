e7ca0dfd0c5d7e6ce5c1288f623b022143e729b4
Segonist <74138556+Segonist@users.noreply.github.com>
2022-01-24 16:50:53 +0200

    Оптимизация под перевод на разные языки

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 import time_to_sec
 import user_data
 import config
+import help_text
 
 bot = commands.Bot(command_prefix=config.settings['PREFIX'], case_insensitive=True)  # префикс, пох на регистр
 bot.remove_command('help')  # удаление команды help для создания своей
@@ -14,62 +15,27 @@ async def on_ready():
 
 @bot.command()
 async def help(ctx):
-    await ctx.send('>>> 📖'
-                   '\nПомощь по боту '
-                   '\nРазделы: '
-                   '\nℹ!mhelp - Основная информация '
-                   '\n📄!chelp - Команды бота '
-                   '\n✏!shelp - Помощь по стилям временных меток '
-                   '\n🚫!ehelp - Частые ошибки')  # команда !help
+    await ctx.send('>>> 📖 ' + help_text.ru_text.get('help'))  # команда !help
 
 
 @bot.command()
 async def mhelp(ctx):
-    return await ctx.send('>>> ℹ '
-                          '\nБот предназначен для создания временных меток следующего типа - <t:1656667800:f> '
-                          '\nТакая метка будет правильно отображаться у всех пользоваетелей, если часовой пояс '
-                          'установлен правильно '
-                          '\nБот работает как на сервере, так и в личных сообщениях.')  # основная помощь
+    return await ctx.send('>>> ℹ ' + help_text.ru_text.get('mhelp'))  # основная помощь
 
 
 @bot.command()
 async def chelp(ctx):
-    return await ctx.send('>>> 📄 '
-                          '\nДля выбора часового пояса введите !utс "часовой пояс" '
-                          '\nПример: !utc +2 '
-                          '\nДля получения временной метки, используйте !time "год" "месяц" "день" "час" "минута" '
-                          '\nПример: !time 2022 01 06 15 20 '
-                          '\n чтобы ее использовать, надо скопировать полученный от бота '
-                          'текст и вставить в нужный чат')  # помощь по командам
+    return await ctx.send('>>> 📄 ' + help_text.ru_text.get('chelp'))  # помощь по командам
 
 
 @bot.command()
 async def shelp(ctx):
-    return await ctx.send('>>> ✏ '
-                          '\nУ временных меток в дискорде есть 7 стилей отображения. '
-                          '\nВнизу указан символ который надо указывать в !style, и пример отображения'
-                          '\nt   <t:1656667800:t> '
-                          '\nT  <t:1656667800:T> '
-                          '\nd  <t:1656667800:d> '
-                          '\nD <t:1656667800:D> '
-                          '\nf   <t:1656667800:f> '
-                          '\nF  <t:1656667800:F> '
-                          '\nR  <t:1656667800:R>')  # помощь по стилям отображения
+    return await ctx.send('>>> ✏ ' + help_text.ru_text.get('shelp'))  # помощь по стилям отображения
 
 
 @bot.command()
 async def ehelp(ctx):
-    return await ctx.send('>>> 🚫 '
-                          '\nP - ошибка '
-                          '\nS - возможное решение '
-                          '\nP: дата/время оттображается не правильно '
-                          '\nS: проверте часовой пояс, !utc; стиль ,!style '
-                          '\nS²: проверте правильно ли вы ввели дату и время команды !time, !chelp '
-                          '\nP: при вводе какой либо команды ничего не проискходит '
-                          '\nS: проверьте правильно ли вы вводите команду '
-                          '\nS²: возможно бот отключен. Статус можно проверить в профиле '
-                          '\nP: любая другая ошибка '
-                          '\nS: обратитесь ко мне в личные сообщения Segonist#4356')  # помощь по ошибкам
+    return await ctx.send('>>> 🚫 ' + help_text.ru_text.get('ehelp'))  # помощь по ошибкам
 
 
 @bot.command()
@@ -103,7 +69,8 @@ async def style(ctx, format_style=None):  # команда style
 
 
 @bot.command(name='time')  # команда !time
-async def _time(ctx, user_year, user_month, user_day, user_hour, user_minute):
+async def _time(ctx, year, month, day, hour, minute):
+    global number_to_withdraw, split
     author = ctx.message.author  # автор сообщения
     if user_data.users_utc.get(author) is None:
         user_utc = 0
@@ -119,14 +86,14 @@ async def _time(ctx, user_year, user_month, user_day, user_hour, user_minute):
         pass
     try:
         if split[0] == '+':
-            number_to_withdraw = time_to_sec.discord_time_plus(user_year, user_month, user_day, user_hour, user_minute,
+            number_to_withdraw = time_to_sec.discord_time_plus(year, month, day, hour, minute,
                                                                int(split[1]), format_style)  # если UTC положительный
         elif split[0] == '-':
-            number_to_withdraw = time_to_sec.discord_time_minus(user_year, user_month, user_day, user_hour, user_minute,
+            number_to_withdraw = time_to_sec.discord_time_minus(year, month, day, hour, minute,
                                                                 int(split[1]), format_style)  # если UTC отрицательный
     except UnboundLocalError:
-        number_to_withdraw = time_to_sec.discord_time(user_year, user_month, user_day, user_hour,
-                                                      user_minute, style)  # если UTC нулевой
+        number_to_withdraw = time_to_sec.discord_time(year, month, day, hour,
+                                                      minute, style)  # если UTC нулевой
     await ctx.send(number_to_withdraw)  # отправка результата
 
 
